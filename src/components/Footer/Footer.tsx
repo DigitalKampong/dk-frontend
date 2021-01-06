9cf3772360b5554450c8f72ebd56eed34354148b
Darren Sim <klassiclegend@gmail.com>
2021-01-06 14:12:37 +0800

    Finish writing footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,12 +12,12 @@ const Footer: React.FunctionComponent = () => {
         <div>
           <div className={styles['image-div']}>
             <a href="http://www.fmas.org.sg/">
-              <img className={styles['image']} src={fmasLogo} alt=""/>
+              <img className={styles['image-fmas']} src={fmasLogo} alt=""/>
             </a>
           </div>
           <div className={styles['image-div']}>
             <a href="https://dsc.comp.nus.edu.sg/">
-              <img className={styles['image']} src={dscLogo} alt=""/>
+              <img className={styles['image-dsc']} src={dscLogo} alt=""/>
             </a>
           </div>
         </div>
