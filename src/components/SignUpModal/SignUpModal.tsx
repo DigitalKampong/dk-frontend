8a88ed9c0aabed8b4f019eef693e0dde78e79bc7
Tianzhen Ni <tianzhen.ni@shopee.com>
2021-05-12 11:48:56 +0800

    Add security questions and change password

--- src/components/SignUpModal/SignUpModal.tsx
+++ src/components/SignUpModal/SignUpModal.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, Input, Modal } from 'semantic-ui-react';
-import { registerUser } from '../../services/user';
+import { Button, Dropdown, Input, Modal } from 'semantic-ui-react';
+import { getSecurityQuestions, registerUser } from '../../services/user';
 import { UPDATE_CURRENT_USER } from '../../store/types';
+import { SecurityQuestion } from '../../types/User';
 import styles from './SignUpModal.module.scss';
 
 type Props = {
@@ -16,7 +17,17 @@ const SignUpModal = (props: Props) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [errorModalOpen, setErrorModalOpen] = useState(false);
+  const [securityQuestions, setSecurityQuestions] = useState<SecurityQuestion[]>([]);
+  const [questionId, setQuestionId] = useState([-1, -1, -1]);
+  const [answers, setAnswers] = useState(['', '', '']);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    getSecurityQuestions().then((response) => {
+      setSecurityQuestions(response.data);
+    });
+  }, []);
   const handleUsernameChange = useCallback((event) => {
     setUsername(event.target.value);
   }, []);
@@ -26,12 +37,36 @@ const SignUpModal = (props: Props) => {
   const handlePasswordChange = useCallback((event) => {
     setPassword(event.target.value);
   }, []);
+  const handleConfirmPasswordChange = useCallback((event) => {
+    setConfirmPassword(event.target.value);
+  }, []);
   const handleSignUpClick = useCallback(() => {
+    if (email === '' || username === '' || password === '' || questionId.some((x) => x === -1) || answers.some((x) => x === '')) {
+      setError('Sign up failed. Please fill in all details and security questions.');
+      setErrorModalOpen(true);
+      return;
+    }
+    if (questionId[0] === questionId[1] || questionId[0] === questionId[2] || questionId[1] === questionId[2]) {
+      setError('Sign up failed. Please do not set duplicated security questions.');
+      setErrorModalOpen(true);
+      return;
+    }
+    if (confirmPassword !== password) {
+      setErrorModalOpen(true);
+      setError('Sign up failed. Two passwords are different.');
+      return;
+    }
     registerUser({
       data: {
         username: username,
         email: email,
         password: password,
+        questionAnswerSet: questionId.map((_, i) => {
+          return {
+            questionId: questionId[i],
+            answer: answers[i],
+          };
+        }),
       },
     })
       .then((response) => {
@@ -42,9 +77,10 @@ const SignUpModal = (props: Props) => {
         setModalOpen(false);
       })
       .catch((error) => {
+        setError('Sign up failed. The same email may have already been registered.');
         setErrorModalOpen(true);
       });
-  }, [email, username, password, dispatch, setModalOpen]);
+  }, [email, username, password, dispatch, setModalOpen, questionId, answers, confirmPassword]);
 
   return (
     <Modal
@@ -60,6 +96,76 @@ const SignUpModal = (props: Props) => {
         <Input className={styles['input-field']} placeholder="Username" value={username} onChange={handleUsernameChange} />
         <Input className={styles['input-field']} placeholder="Email" value={email} onChange={handleEmailChange} />
         <Input className={styles['input-field']} placeholder="Password" type="password" value={password} onChange={handlePasswordChange} />
+        <Input
+          className={styles['input-field']}
+          placeholder="Confirm password"
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+        <Dropdown
+          className={styles['input-field']}
+          placeholder="Select security question 1"
+          fluid
+          selection
+          options={securityQuestions.map((qn) => {
+            return { key: qn.id, text: qn.content, value: qn.id };
+          })}
+          value={questionId[0]}
+          onChange={(_, target) => {
+            setQuestionId(questionId.map((v, i) => (i === 0 ? (target.value as number) : v)));
+          }}
+        />
+        <Input
+          className={styles['input-field']}
+          placeholder="Answer 1"
+          value={answers[0]}
+          onChange={(event) => {
+            setAnswers(answers.map((ans, i) => (i === 0 ? event.target.value : ans)));
+          }}
+        />
+        <Dropdown
+          className={styles['input-field']}
+          placeholder="Select security question 2"
+          fluid
+          selection
+          options={securityQuestions.map((qn) => {
+            return { key: qn.id, text: qn.content, value: qn.id };
+          })}
+          value={questionId[1]}
+          onChange={(_, target) => {
+            setQuestionId(questionId.map((v, i) => (i === 1 ? (target.value as number) : v)));
+          }}
+        />
+        <Input
+          className={styles['input-field']}
+          placeholder="Answer 2"
+          value={answers[1]}
+          onChange={(event) => {
+            setAnswers(answers.map((ans, i) => (i === 1 ? event.target.value : ans)));
+          }}
+        />
+        <Dropdown
+          className={styles['input-field']}
+          placeholder="Select security question 3"
+          fluid
+          selection
+          options={securityQuestions.map((qn) => {
+            return { key: qn.id, text: qn.content, value: qn.id };
+          })}
+          value={questionId[2]}
+          onChange={(_, target) => {
+            setQuestionId(questionId.map((v, i) => (i === 2 ? (target.value as number) : v)));
+          }}
+        />
+        <Input
+          className={styles['input-field']}
+          placeholder="Answer 3"
+          value={answers[2]}
+          onChange={(event) => {
+            setAnswers(answers.map((ans, i) => (i === 2 ? event.target.value : ans)));
+          }}
+        />
         <Button className={styles['signup-button']} onClick={handleSignUpClick}>
           Sign up
         </Button>
@@ -70,7 +176,7 @@ const SignUpModal = (props: Props) => {
         onClose={() => setErrorModalOpen(false)}
         onOpen={() => setErrorModalOpen(true)}
         open={errorModalOpen}
-        content="Sign up failed. The same email may have already been registered."
+        content={error}
         actions={['Ok']}
       />
     </Modal>
